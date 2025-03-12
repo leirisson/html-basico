a7840ec49d6f230c0e14b6ca153eeb0371bb55d1
leirisson <leirissonsouza99@gmail.com>
2025-03-12 12:02:50 -0400

    FALSY e TRUTHY exemplo do que é considerado falso e verdadeiro em javascript

---
+++ 14-javaScript/2-FALSY-TRUTHY/script.js
@@ -0,0 +1,21 @@
+
+
+console.log("### EXEMPLOS DE FALSY ###") // falso
+console.log(false ? 'verdadeiro' : 'falso')
+console.log(0 ? 'verdadeiro' : 'falso')
+console.log(-0 ? 'verdadeiro' : 'falso')
+console.log('' ? 'verdadeiro' : 'falso')
+console.log(null ? 'verdadeiro' : 'falso')
+console.log(undefined ? 'verdadeiro' : 'falso')
+console.log(NaN ? 'verdadeiro' : 'falso')
+
+
+console.log("### EXEMPLOS DE TRUTHY ###") // verdadeiro
+console.log(true ? 'verdadeiro' : 'falso')
+console.log({} ? 'verdadeiro' : 'falso')
+console.log([] ? 'verdadeiro' : 'falso')
+console.log(1 ? 'verdadeiro' : 'false')
+console.log(3.32 ? 'verdadeiro' :'falso')
+console.log("0" ? 'verdadeiro' :'falso')
+console.log(" " ? 'verdadeiro' :'falso')
+console.log("Leirisson" ? 'verdadeiro' :'falso')
